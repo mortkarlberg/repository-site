73f97be698156625f234bd6c7734395a4e46fc94
Mårten Karlberg <mortkarlberg@gmail.com>
2019-03-15 19:57:12 +0100

    Refactorization

--- README.md
+++ README.md
@@ -27,5 +27,6 @@ Dev release: https://repository-203818.appspot.com
 
 ### Start script:
 * npm install
-* start jsx-preprocessor: npx babel --watch src --out-dir www/js --presets react-app/prod  (https://reactjs.org/docs, add-react-to-a-website.html)
+* start jsx-preprocessor: npx babel --watch src/jsx --out-dir www/js --presets react-app/prod  (https://reactjs.org/docs, add-react-to-a-website.html)
 * start sass-preprocessor: scss - npm install -g sass, sass --watch src/sass:www/css
+* TODO: copy html from src to www on change
--- src/jsx/terminal.jsx
+++ src/jsx/terminal.jsx
@@ -22,15 +22,12 @@ class Terminal extends React.Component {
 // Set the cursor position of the "#terminal-input" element to the end when the page loads.
 // Credits: http://blog.vishalon.net/index.php/javascript-getting-and-setting-caret-position-in-textarea/
 function setCaretPosition(ctrl, pos) {
-  console.log(pos);
   // Modern browsers
   if (ctrl.setSelectionRange) {
-    console.log("modern:", pos);
     ctrl.focus();
     ctrl.setSelectionRange(pos, pos);
   // IE8 and below
   } else if (ctrl.createTextRange) {
-    console.log("ie8:", pos);
     var range = ctrl.createTextRange();
     range.collapse(true);
     range.moveEnd('character', pos);
@@ -42,6 +39,6 @@ function setCaretPosition(ctrl, pos) {
 const container = document.querySelector('#terminal');
 const terminal = React.createElement(Terminal);
 ReactDOM.render(terminal, container);
-const input = document.querySelector('#terminal-input');
-console.log(input.textContent, input.textContent.length);
-setCaretPosition(input, input.textContent.length);
+//const input = document.querySelector('#terminal-input');
+//console.log(input.textContent, input.textContent.length);
+//setCaretPosition(input, input.textContent.length);

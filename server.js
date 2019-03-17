538181a0330c169eb3015a26e6a302b82d0e6399
Mårten Karlberg <mortkarlberg@gmail.com>
2019-03-17 21:15:28 +0100

    Parse command from path param and body as json

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const path = require("path");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
 app.use(express.static("www"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.post("/terminal/commands", (req, res) => {
-    console.log("Incoming terminal command. Request:", req);
+app.post("/terminal/commands/:command", (req, res) => {
+    console.log("Incoming terminal command:", req.params.command, req.body);
     //TODO handle request
     //TODO write back response
 });
